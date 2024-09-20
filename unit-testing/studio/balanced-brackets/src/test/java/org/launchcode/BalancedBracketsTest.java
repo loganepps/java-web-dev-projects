a6ae95f01caae51789e0753e956d9eaccd7de7c5
loganepps <logepps@gmail.com>
2024-09-19 23:12:31 -0500

    class 5 studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,52 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void onlyBracketsOnlyOpen(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+    @Test
+    public void onlyBracketsBackwards(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]LaunchCode["));
+    }
+    @Test
+    public void onlyBracketsExtraOpen(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[LaunchCode]"));
+    }
+    @Test
+    public void onlyBracketsOnlyClosing(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode]"));
+    }
+    @Test
+    public void onlyBracketsNested(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launch[Code]]"));
+    }
+    @Test
+    public void onlyBracketsEachCharInBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[L][a][u][n][c][h][C][o][d][e]"));
+    }
+    @Test
+    public void onlyBracketsExtraClose(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[LaunchCode]]]"));
+    }
+    @Test
+    public void onlyBracketsEmptyString(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+    @Test
+    public void onlyBracketsBackwardsMiddle(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+    @Test
+    public void onlyBracketsOnlyBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+
 }
